739d14e25fd63d6503e83e47628a2743bbf97cbb
Cokemonkey11 <Cokemonkey11@gmail.com>
2016-07-17 19:30:40 +0100

    progress

--- generate.py
+++ generate.py
@@ -1,31 +1,46 @@
 
-import os
+import os, sys
 
-import json
+import json, datetime, time
 
 from subprocess import call
 
-from git import Repo
+from git         import Repo, NoSuchPathError
+from PIL         import Image
+from imgurpython import ImgurClient
 
 
 if __name__ == "__main__":
+    active  = Image.open('resource/bm_btn.png')
+    passive = Image.open('resource/bm_pas.png')
+
+    now = datetime.datetime.fromtimestamp(time.time())
+
+    files = []
+    uris  = []
+
     with open('config.json', 'r') as cfg:
         config = json.load(cfg)
 
-        for repo in config["repositories"]:
+        num_repos_size = len(str(len(config["repositories"])))
+        active_age_max = int(config["days-for-active"])
+        preview_path   = config["preview-path"]
+
+        for index, repo in enumerate(config["repositories"]):
             simple_name = repo.split('/')[1].split('.git')[0]
 
-            r = Repo.init('gen/' + simple_name, bare=True)
-            origin = r.create_remote('origin', repo)
+            tag = "[" + str(index + 1) + "/" + str(len(config["repositories"])).rjust(num_repos_size, ' ') + "]"
+            print tag + " Cloning shallow copy " + simple_name.ljust(20, ' ') + " - please wait a few minutes."
+            sys.stdout.flush()
 
-            cw = r.config_writer()
-            #cw.set('core.bare', 'false')
-            cw.set('core.worktree', 'gen/' + simple_name)
-            cw.release()
+            try:
+                r = Repo('gen/' + simple_name)
+            except NoSuchPathError:
+                r = Repo.clone_from(repo, 'gen/' + simple_name, depth=1)
+
+            origin = r.remotes.origin
 
             cw = origin.config_writer
-            #cw.set('core.bare', 'false')
-            #cw.set('core.worktree', 'gen/' + simple_name)
             cw.set('sparseCheckout', 'true')
             cw.release()
 
@@ -33,6 +48,32 @@ if __name__ == "__main__":
             if not os.path.exists(os.path.dirname(fname)):
                 os.makedirs(os.path.dirname(fname))
             with open(fname, 'w') as sparse:
-                sparse.write(simple_name + '/' + config['preview-path'])
+                sparse.write(simple_name + '/' + preview_path)
 
             origin.pull(depth=1)
+
+            date = datetime.datetime.fromtimestamp(int(r.rev_parse('head').committed_date))
+            if (now - date).days > active_age_max:
+                border = passive
+            else:
+                border = active
+
+            if os.path.isfile( 'gen/' + simple_name + '/' + preview_path):
+                f = Image.open('gen/' + simple_name + '/' + preview_path).resize((128, 128)).convert('RGBA')
+                Image.alpha_composite(f, border).save('gen/' + simple_name + '.png', "PNG")
+                files.append('gen/' + simple_name + '.png')
+            else:
+                print "  Don't have preview... skipping."
+
+        print "Accessing imgur API."
+        client = ImgurClient(config['imgur']['client-id'], config['imgur']['client-secret'])
+        for path in files:
+            print "  Uploading " + path + " ."
+            uris.append(client.upload_from_path(path)['link'])
+
+        with open('out.vbc', 'w') as f:
+            print "Writing VB code file."
+            for uri in uris:
+                f.write('[img]' + uri + '[/img]')
+
+        print "Done.\n\n"
