37c7054356c07255d388220fe9e72b724a3b032e
Cokemonkey11 <Cokemonkey11@gmail.com>
2017-02-24 21:12:25 +0000

    use py3 style prints

--- generate.py
+++ generate.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         simple_name = repo[0].split('/')[1].split('.git')[0]
 
         tag = "[" + str(index + 1) + "/" + str(len(config["repositories"])).rjust(num_repos_size, ' ') + "]"
-        print tag + " Cloning shallow copy " + simple_name.ljust(20, ' ') + " - please wait a few minutes."
+        print(tag + " Cloning shallow copy " + simple_name.ljust(20, ' ') + " - please wait a few minutes.")
         sys.stdout.flush()
 
         try:
@@ -64,17 +64,17 @@ if __name__ == "__main__":
             Image.alpha_composite(f, border).save('gen/' + simple_name + '.png', "PNG")
             files.append(('gen/' + simple_name + '.png', repo[1], age))
         else:
-            print "  Don't have preview... skipping."
+            print("  Don't have preview... skipping.")
 
-    print "Accessing imgur API."
+    print("Accessing imgur API.")
     client = ImgurClient(config['imgur']['client-id'], config['imgur']['client-secret'])
     for path in sorted(files, key=lambda x: x[2]):
-        print "  Uploading " + path[0] + " ."
+        print("  Uploading " + path[0] + " .")
         uris.append((client.upload_from_path(path[0])['link'], path[1]))
 
     with open('out.vbc', 'w') as f:
-        print "Writing VB code file."
+        print("Writing VB code file.")
         for uri in uris:
             f.write('[url=' + uri[1] + '][img]' + uri[0] + '[/img][/url]')
 
-    print "Done.\n\n"
+    print("Done.\n\n")
